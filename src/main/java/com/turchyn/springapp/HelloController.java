fe53c7441890aaec64eeb7789edb315c4e380a8d
oleh-turchyn <47058088+Leggi19@users.noreply.github.com>
2020-08-29 23:40:33 +0300

    add new dependencies and java config

---
+++ src/main/java/com/turchyn/springapp/HelloController.java
@@ -0,0 +1,12 @@
+package com.turchyn.springapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello-world";
+    }
+}
